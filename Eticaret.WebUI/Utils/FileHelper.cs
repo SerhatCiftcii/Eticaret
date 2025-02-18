adc1638f568f68dfa9a7f9fccb7eade6f1242356
Serhat <s.ciftci561@gmail.com>
2025-02-18 17:46:03 +0300

    file-loader file remover ve marka logo ekleme islemleri tamamlandi

---
+++ Eticaret.WebUI/Utils/FileHelper.cs
@@ -0,0 +1,34 @@
+﻿namespace Eticaret.WebUI.Utils
+{
+    public class FileHelper
+    {
+        //sunucya dosya yükleme 
+        public static async Task<string> FileLoaderAsync(IFormFile formFile, string filePath = "/Img/")
+        {
+            string fileName = "";
+            if (formFile != null && formFile.Length > 0)
+            {
+                fileName = formFile.FileName.ToLower();//dosya adını küçük harfe çevirir
+                string directory = Directory.GetCurrentDirectory() + "/wwwroot/" + filePath + fileName;
+
+                using var stream = new FileStream(directory, FileMode.Create);
+                await formFile.CopyToAsync(stream);
+            }
+
+            return fileName;
+        }
+        //sunucadan dosya silme
+        public static bool FileRemover(string fileName, string filePath = "/Img/")
+        {  
+                string directory = Directory.GetCurrentDirectory() + "/wwwroot" + filePath + fileName;
+                if (File.Exists(directory))
+                {
+                    File.Delete(directory);
+                    return true;
+                }
+                return false;
+
+            }
+        }
+    }
+
