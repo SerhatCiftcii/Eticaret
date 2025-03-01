95510137850fa05a13733d995188f35d57cf2bba
Serhat <s.ciftci561@gmail.com>
2025-03-01 17:31:45 +0300

    anasayfa açılır kategori ve admin sayfa düzenlemeri iyilestirild

--- Eticaret.Service/obj/Debug/net8.0/Eticaret.Service.AssemblyInfo.cs
+++ Eticaret.Service/obj/Debug/net8.0/Eticaret.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Eticaret.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b651491e38989c40bce6e50b708b51d28f31656")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72b363a7b606cbcbcf6046d5ca3d7b220b706c21")]
 [assembly: System.Reflection.AssemblyProductAttribute("Eticaret.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Eticaret.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Eticaret.WebUI/Areas/Admin/Controllers/CategoriesController.cs
+++ Eticaret.WebUI/Areas/Admin/Controllers/CategoriesController.cs
@@ -44,38 +44,44 @@ namespace Eticaret.WebUI.Areas.Admin.Controllers
         // GET: Admin/Categories/Create
         public async Task<IActionResult> Create()
         {
-            ViewBag.Kategoriler = new SelectList(await _context.Categories.ToListAsync(),"Id","Name");
+            var anaKategoriler = await _context.Categories
+      .Where(c => c.ParentId == 0)
+      .ToListAsync();
+
+            ViewBag.Kategoriler = new SelectList(anaKategoriler, "Id", "Name");
             return View();
+
         }
 
-       
+
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Category category,IFormFile? Image)
+        public async Task<IActionResult> Create(Category category, IFormFile? Image)
         {
             if (ModelState.IsValid)
             {
-                category.Image = await FileHelper.FileLoaderAsync(Image, "/Img/Categories/");
-                await _context.AddAsync(category);
+                category.Image = await FileHelper.FileLoaderAsync(Image,"/Img/Categories/");
+                _context.Add(category);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
 
-            ViewBag.Kategoriler = new SelectList(await _context.Categories.ToListAsync(), "Id", "Name");
-         
+            var anaKategoriler = await _context.Categories
+               .Where(c => c.ParentId == 0)
+               .ToListAsync();
+
+            ViewBag.Kategoriler = new SelectList(anaKategoriler, "Id", "Name");
             return View(category);
         }
 
         // GET: Admin/Categories/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-
             if (id == null)
             {
                 return NotFound();
             }
-
 
             var category = await _context.Categories.FindAsync(id);
             if (category == null)
@@ -83,15 +89,21 @@ namespace Eticaret.WebUI.Areas.Admin.Controllers
                 return NotFound();
             }
 
-            ViewBag.Kategoriler = new SelectList(await _context.Categories.ToListAsync(), "Id", "Name");
+            // Ana kategorileri çek ve mevcut parent ID'yi seçili yap
+            var anaKategoriler = await _context.Categories
+                                              .Where(c => c.ParentId == 0 || c.Id == category.ParentId)
+                                              .ToListAsync();
+
+            ViewBag.Kategoriler = new SelectList(anaKategoriler, "Id", "Name", category.ParentId);
             return View(category);
         }
 
-       
+
+
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id,Category category,IFormFile? Image, bool DeleteLogo = false)
+        public async Task<IActionResult> Edit(int id, Category category, IFormFile? Image, bool DeleteLogo = false)
         {
             if (id != category.Id)
             {
@@ -102,22 +114,25 @@ namespace Eticaret.WebUI.Areas.Admin.Controllers
             {
                 try
                 {
-                    if(DeleteLogo)
+                    // Kendini parent seçmesini engelle
+                    if (category.ParentId == category.Id)
                     {
-                        
+                        ModelState.AddModelError(string.Empty, "Bir ana kategori kategori kendisini üst kategori olarak seçemez.");
+                        return View(category);
+                    }
+                    if (DeleteLogo)
+                    {
                         category.Image = string.Empty;
                     }
-                    
-                   if (Image is not null) //
-                    {
-                        category.Image = await FileHelper.FileLoaderAsync(Image,"/Img/Categories/");
-                    }
+                    if (Image is not null)
+                        category.Image = await FileHelper.FileLoaderAsync(Image, "/Img/Categories/");
+
                     _context.Update(category);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!CategoryExists(category.Id))
+                    if (!_context.Categories.Any(c => c.Id == category.Id))
                     {
                         return NotFound();
                     }
@@ -129,10 +144,15 @@ namespace Eticaret.WebUI.Areas.Admin.Controllers
                 return RedirectToAction(nameof(Index));
             }
 
-            ViewBag.Kategoriler = new SelectList(await _context.Categories.ToListAsync(), "Id", "Name");
-          
+            var anaKategoriler = await _context.Categories
+                                              .Where(c => c.ParentId == 0 || c.Id == category.ParentId)
+                                              .ToListAsync();
+
+            ViewBag.Kategoriler = new SelectList(anaKategoriler, "Id", "Name", category.ParentId);
             return View(category);
         }
+
+
 
         // GET: Admin/Categories/Delete/5
         public async Task<IActionResult> Delete(int? id)
