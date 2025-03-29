d1cf8a386be3477442db06579e74a6c44e121f07
Serhat <s.ciftci561@gmail.com>
2025-03-30 01:47:26 +0300

    Uye Kayit surec duzenlemesi,Kullanici adres kontrolu,Liste sayfası,adres ekleme islemi

--- Eticaret.WebUI/Models/CheckoutViewModel.cs
+++ Eticaret.WebUI/Models/CheckoutViewModel.cs
@@ -6,5 +6,7 @@ namespace Eticaret.WebUI.Models
     {
         public List<CartLine>? CartProducts { get; set; }
         public decimal TotalPrice { get; set; }
+
+        public List<Address>? Addresses { get; set; }
     }
 }
