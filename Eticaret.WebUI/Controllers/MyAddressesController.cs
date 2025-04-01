750da94b17a7fbdca1b8e07141bfdac0958fece9
Serhat <s.ciftci561@gmail.com>
2025-04-01 23:27:04 +0300

    other adres yani teslimat adresi ile fatura adresi aynı secilemez bu yuzden update ettim otheraddresses Edit Isleminde

--- Eticaret.WebUI/Controllers/MyAddressesController.cs
+++ Eticaret.WebUI/Controllers/MyAddressesController.cs
@@ -70,11 +70,53 @@ namespace Eticaret.WebUI.Controllers
                 return NotFound("Kullanıcı Datası Bulunamadı! Lütfen Tekrar Giriş Yapın!");
             }
 
-            var model= await _serviceAddress.GetAsync(u=>u.AddressGuid.ToString() == id);
+            var model= await _serviceAddress.GetAsync(u=>u.AddressGuid.ToString() == id && u.AppUserId==appUser.Id);
+            if(model == null)
+                return NotFound("Adres Bilgisi Bulunamadı! Lütfen Tekrar Giriş Yapın!");
             return View(model);
         }
-        
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public async Task<IActionResult> Edit(string id,Address address)
+        {
+            var appUser = await _serviceAppUser.GetAsync(x => x.UserGuid.ToString() == HttpContext.User.FindFirst("UserGuid").Value);
+            if (appUser == null)
+            {
+                return NotFound("Kullanıcı Datası Bulunamadı! Lütfen Tekrar Giriş Yapın!");
+            }
 
+            var model = await _serviceAddress.GetAsync(u => u.AddressGuid.ToString() == id && u.AppUserId == appUser.Id);
+            if (model == null)
+                return NotFound("Kullanıcı Datası Bulunamadı! Lütfen Tekrar Giriş Yapın!");
+            model.Title= address.Title;
+            model.Destrict=address.Destrict;
+            model.City=address.City;
+            model.OpenAddress=address.OpenAddress;
+            model.IsDeliveryAddress=address.IsDeliveryAddress;
+            model.IsBillingAddress=address.IsBillingAddress;
+            model.IsActive=address.IsActive;
+            //fatura adresi ile teslimat adresi aynı anda olamaz other adresle düzelttim.
+            var otherAddresses= await _serviceAddress.GetAllAsync(x=>x.AppUserId==appUser.Id && x.Id != model.Id);
+            foreach (var otherAddress in otherAddresses)
+            {
+                otherAddress.IsDeliveryAddress = false;
+                otherAddress.IsBillingAddress = false;
+                _serviceAddress.Update(otherAddress);
+            }
+            try
+            {
+                _serviceAddress.Update(model);
+                await _serviceAddress.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+
+                ModelState.AddModelError("", "Hata Oluştu Kullanıcı Adres Sayfası Edit");
+            }
+            return View(model);
         }
+
+    }
     }
 
