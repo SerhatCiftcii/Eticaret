0c851a8cea84a8edd8ba5b17150d00f140f14ba9
Serhat <s.ciftci561@gmail.com>
2025-04-30 21:11:49 +0300

    Iyzco paket yukleme, kod entegrasyonu

--- Eticaret.WebUI/Controllers/CartController.cs
+++ Eticaret.WebUI/Controllers/CartController.cs
@@ -4,9 +4,13 @@ using Eticaret.Service.Abstract;
 using Eticaret.Service.Concrete;
 using Eticaret.WebUI.ExtensionMethods;
 using Eticaret.WebUI.Models;
+using Iyzipay;
+using Iyzipay.Model;
+using Iyzipay.Request;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.CodeAnalysis;
+using System.Drawing;
 
 namespace Eticaret.WebUI.Controllers
 {
@@ -14,12 +18,12 @@ namespace Eticaret.WebUI.Controllers
     {
         private readonly IService<AppUser> _serviceAppUser;
         private readonly IService<Product> _serviceProduct;
-        private readonly IService<Address> _seriveceAddress;
+        private readonly IService<Core.Entities.Address> _seriveceAddress;
         private readonly IService<Order> _seriveceOrder;
         private readonly IEmailService _emailService;  // Burayı doğru şekilde tanımlıyoruz
 
         public CartController(IService<Product> serviceProduct,
-                              IService<Address> serviceAddress,
+                              IService<Core.Entities.Address> serviceAddress,
                               IService<AppUser> serviceAppUser,
                               IService<Order> seriveceOrder,
                               IEmailService emailService)  // IEmailService parametre olarak alıyoruz
@@ -143,7 +147,7 @@ namespace Eticaret.WebUI.Controllers
 
             var teslimatAdresi = addresses.FirstOrDefault(a => a.AddressGuid.ToString() == DeliveryAddress); // Teslimat adresi
             var faturaAdresi = addresses.FirstOrDefault(a => a.AddressGuid.ToString() == BillingAddress); // Fatura adresi
-
+            //Ödeme Çekme iyizco 
             // Sipariş nesnesi oluşturuluyor
             var siparis = new Order
             {
@@ -168,7 +172,95 @@ namespace Eticaret.WebUI.Controllers
                     UnitPrice = item.Product.Price,
                 });
             }
+            #region OdemIslemi
+            Options options = new Options();
+            options.ApiKey = "your api key";
+            options.SecretKey = "your secret key";
+            options.BaseUrl = "https://sandbox-api.iyzipay.com";
 
+            CreatePaymentRequest request = new CreatePaymentRequest();
+            request.Locale = Locale.TR.ToString();
+            request.ConversationId = "123456789";
+            request.Price = "1";
+            request.PaidPrice = "1.2";
+            request.Currency = Currency.TRY.ToString();
+            request.Installment = 1;
+            request.BasketId = "B67832";
+            request.PaymentChannel = PaymentChannel.WEB.ToString();
+            request.PaymentGroup = PaymentGroup.PRODUCT.ToString();
+
+            PaymentCard paymentCard = new PaymentCard();
+            paymentCard.CardHolderName = "John Doe";
+            paymentCard.CardNumber = "5528790000000008";
+            paymentCard.ExpireMonth = "12";
+            paymentCard.ExpireYear = "2030";
+            paymentCard.Cvc = "123";
+            paymentCard.RegisterCard = 0;
+            request.PaymentCard = paymentCard;
+
+            Buyer buyer = new Buyer();
+            buyer.Id = "BY789";
+            buyer.Name = "John";
+            buyer.Surname = "Doe";
+            buyer.GsmNumber = "+905350000000";
+            buyer.Email = "email@email.com";
+            buyer.IdentityNumber = "74300864791";
+            buyer.LastLoginDate = "2015-10-05 12:43:35";
+            buyer.RegistrationDate = "2013-04-21 15:12:09";
+            buyer.RegistrationAddress = "Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1";
+            buyer.Ip = "85.34.78.112";
+            buyer.City = "Istanbul";
+            buyer.Country = "Turkey";
+            buyer.ZipCode = "34732";
+            request.Buyer = buyer;
+
+            var shippingAddress = new Iyzipay.Model.Address();
+            shippingAddress.ContactName = "Jane Doe";
+            shippingAddress.City = "Istanbul";
+            shippingAddress.Country = "Turkey";
+            shippingAddress.Description = "Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1";
+            shippingAddress.ZipCode = "34742";
+            request.ShippingAddress = shippingAddress;
+
+            var billingAddress = new Iyzipay.Model.Address();
+            billingAddress.ContactName = "Jane Doe";
+            billingAddress.City = "Istanbul";
+            billingAddress.Country = "Turkey";
+            billingAddress.Description = "Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1";
+            billingAddress.ZipCode = "34742";
+            request.BillingAddress = billingAddress;
+
+            List<BasketItem> basketItems = new List<BasketItem>();
+            BasketItem firstBasketItem = new BasketItem();
+            firstBasketItem.Id = "BI101";
+            firstBasketItem.Name = "Binocular";
+            firstBasketItem.Category1 = "Collectibles";
+            firstBasketItem.Category2 = "Accessories";
+            firstBasketItem.ItemType = BasketItemType.PHYSICAL.ToString();
+            firstBasketItem.Price = "0.3";
+            basketItems.Add(firstBasketItem);
+
+            BasketItem secondBasketItem = new BasketItem();
+            secondBasketItem.Id = "BI102";
+            secondBasketItem.Name = "Game code";
+            secondBasketItem.Category1 = "Game";
+            secondBasketItem.Category2 = "Online Game Items";
+            secondBasketItem.ItemType = BasketItemType.VIRTUAL.ToString();
+            secondBasketItem.Price = "0.5";
+            basketItems.Add(secondBasketItem);
+
+            BasketItem thirdBasketItem = new BasketItem();
+            thirdBasketItem.Id = "BI103";
+            thirdBasketItem.Name = "Usb";
+            thirdBasketItem.Category1 = "Electronics";
+            thirdBasketItem.Category2 = "Usb / Cable";
+            thirdBasketItem.ItemType = BasketItemType.PHYSICAL.ToString();
+            thirdBasketItem.Price = "0.2";
+            basketItems.Add(thirdBasketItem);
+            request.BasketItems = basketItems;
+
+            Payment payment = await Payment.Create(request, options);
+            #endregion
             try
             {
                 // Siparişi veritabanına kaydediyoruz
